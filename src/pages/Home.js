1f5921a63f9e558cdef764cf3bcca4d0d0beff6b
Atharv <atharvbhandare2001@gmail.com>
2022-02-07 15:57:03 +0530

    connecting firebase to Home page and collecting all documents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import BookList from '../components/BookList'
-import BookForm from '../components/BookForm'
+import { useEffect, useState } from "react";
+import BookList from "../components/BookList";
+import BookForm from "../components/BookForm";
+
+// firestore db
+import { db } from "../firebase/config";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function Home() {
-  const [books, setBooks] = useState([
-    { title: 'the name of the wind', id: 1 },
-    { title: 'the dragon reborn', id: 2 },
-    { title: 'the final empire', id: 3 },
-    { title: 'the way of kings', id: 4 }
-  ])
+  const [books, setBooks] = useState([]);
 
+  useEffect(() => {
+    const ref = collection(db, "books");
+
+    getDocs(ref).then((snapshot) => {
+      let results = [];
+      //where snapshot.docs method is used to get all the documents from the collection 'books'
+      snapshot.docs.forEach((doc) => {
+        results.push({ ...doc.data(), id: doc.id });
+        console.log(doc.id, " => ", doc.data());
+      });
+      setBooks(results);
+    });
+  }, []);
   return (
     <div className="App">
       {books && <BookList books={books} />}
