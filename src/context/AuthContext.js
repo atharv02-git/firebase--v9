8d3e1ba68a46f05acc58ececb0cfca71b4279ad1
Atharv <atharvbhandare2001@gmail.com>
2022-02-08 15:24:05 +0530

    adding auth context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,40 @@
+import { createContext, useEffect, useReducer } from "react";
+import { auth } from "../firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
+export const AuthContext = createContext();
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    case "AUTH_IS_READY":
+      return { ...state, user: action.payload, authIsReady: true };
+    default:
+      break;
+  }
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    // now checking if the user is still logged in after the page refresh, so the initial value is false it means the user is not logged in
+    authIsReady: false,
+  });
+  // useEffect runs only once when the the auth state changes
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged((auth, user) => {
+      dispatch({ type: "AUTH_IS_READY", payload: user });
+      unSubscribe();
+    });
+  }, []);
+  console.log("AuthContext state:", state);
+
+  return (
+    //   later on when we create custom hook to control sigup login logout then dispatch hook can be used to update our context values
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
